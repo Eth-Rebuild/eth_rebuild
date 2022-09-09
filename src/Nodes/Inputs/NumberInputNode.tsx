258bde11b0c952697d23cc65f40b252c6614ee05
mercuricchloride <mercuryprotocol@protonmail.com>
2022-09-09 14:25:43 -0700

    input reformat

--- src/Nodes/Inputs/NumberInputNode.tsx
+++ src/Nodes/Inputs/NumberInputNode.tsx
@@ -2,21 +2,19 @@ import { Handle, Position } from "react-flow-renderer";
 import { useRecoilState } from "recoil";
 import { createHandles } from "../../Helpers/helpers";
 import { nodeDataState } from "../../Recoil/Atoms/atoms";
+import { InputNumber } from "antd";
 
 export function NumberInputNode({ id }) {
   const [state, setState] = useRecoilState(nodeDataState(id));
 
+  const onChange = (value: number) => {
+    setState({ a: value });
+  };
+
   return (
     <div className="custom-node">
       <h4>Number Input</h4>
-      <input
-        type="number"
-        onChange={(e) => {
-          setState({
-            a: e.target.valueAsNumber || 0,
-          });
-        }}
-      />
+      <InputNumber onChange={onChange} defaultValue={0} />
       {createHandles("output", 1)}
     </div>
   );
--- src/Nodes/Inputs/StringInputNode.tsx
+++ src/Nodes/Inputs/StringInputNode.tsx
@@ -1,7 +1,7 @@
-import { Handle, Position } from "react-flow-renderer";
 import { useRecoilState } from "recoil";
 import { createHandles } from "../../Helpers/helpers";
 import { nodeDataState } from "../../Recoil/Atoms/atoms";
+import { Input } from "antd";
 
 export function StringInputNode({ id }) {
   const [state, setState] = useRecoilState(nodeDataState(id));
@@ -9,13 +9,7 @@ export function StringInputNode({ id }) {
   return (
     <div className="custom-node">
       <h4>String Input</h4>
-      <input
-        type="string"
-        defaultValue=""
-        onChange={(e) => {
-          setState({ a: e.target.value });
-        }}
-      />
+      <Input onChange={(e) => setState({ a: e.target.value })} />
       {createHandles("output", 1)}
     </div>
   );
