8ad3ebadc7214dc7937b49ba552923372da16f9f
mercuricchloride <mercuryprotocol@protonmail.com>
2022-10-27 11:28:00 -0700

    added blockies for string inputs

--- src/Nodes/Inputs/StringInputNode.tsx
+++ src/Nodes/Inputs/StringInputNode.tsx
@@ -2,13 +2,28 @@ import { useRecoilState } from "recoil";
 import { Handles } from "../../Helpers/helpers";
 import { nodeDataState } from "../../Recoil/Atoms/atoms";
 import { Input } from "antd";
+import { isAddress } from "ethers/lib/utils";
+import makeBlockie from "ethereum-blockies-base64";
 
 export function StringInputNode({ id }) {
   const [state, setState] = useRecoilState(nodeDataState(id));
+  const isValid = isAddress(state?.["a"]);
 
   return (
     <div className="custom-node input">
       <h4>String Input</h4>
+      {isValid ? (
+        <img
+          style={{
+            justifySelf: "flex-end",
+          }}
+          height={30}
+          width={30}
+          src={makeBlockie(state.a)}
+        />
+      ) : (
+        ""
+      )}
       <Input className="input" onChange={(e) => setState((oldState) => ({ ...oldState, a: e.target.value }))} />
       <Handles
         kind="output"
